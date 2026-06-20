9c9fb67ce2ff17c73c3614ce3456ab5372cb88dd
Bouhaik anes mohammed el amine <78903844+S1nju@users.noreply.github.com>
2026-06-20 20:49:38 +0100

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,12 @@
+from functools import cache
+class Solution:
+    @cache
+    def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 1
+        if n ==1 :
+            return 1
+        
+        
+        return self.climbStairs(n-1)+ self.climbStairs(n-2)
+        
